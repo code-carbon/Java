56959795bcb805f5e5fbea5125f91cd24eafcf2c
codecarbon <71373929+code-carbon@users.noreply.github.com>
2020-09-17 12:33:12 +0530

    Update factor.java

--- factor.java
+++ factor.java
@@ -1,15 +1,14 @@
-package math_works;
+package Math_works;
 import java.util.Scanner;
-public class factor {
+public class Factors_of_a_number {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int number = sc.nextInt();
     System.out.print("Factors of " + number + " are: ");
         for(int i = 1; i <= number; ++i) {
             if (number % i == 0) {
-                System.out.print(i + " ");
+                System.out.print(i + ", ");
             }
-        
     }
   }
 }
